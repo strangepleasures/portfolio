f58c041ac5d38e8da85168a44bcb54ed01d40b5a
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2024-07-21 06:24:07 +0200

    Updates

--- src/props.ts
+++
@@ -1,8 +0,0 @@
-export type NavBarProps = {
-    activeProject: number;
-    setActiveProject: React.Dispatch<React.SetStateAction<number>>;
-};
-
-export type DisplayAreaProps = {
-    activeProject: number;
-};
